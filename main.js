0ac16a69a43d0ab68fd3c5bbaa0a32e9399a72ca
BenteKavsek <35726@ma-web.nl>
2023-01-07 16:16:23 +0100

    Header klaar

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
 // kijken of pagina geladen is
 window.addEventListener("load", () => {
 
-    // var gemaakt hoever gescrolled is
-    let lastKnownScrollPosition = 0;
+  // var gemaakt hoever gescrolled is
+  let lastKnownScrollPosition = 0;
 
-    const header=document.querySelector("#header");
+  const header = document.querySelector("#header");
 
-    const headerHeight=header.clientHeight ;
+  const headerHeight = header.clientHeight;
 
-    // kijken of er gescrolled word
-    document.addEventListener("scroll", () => {
+  // kijken of er gescrolled word
+  document.addEventListener("scroll", () => {
 
-        // var aanpassen naar scroll punt
-        lastKnownScrollPosition = window.scrollY;
+    // var aanpassen naar scroll punt
+    lastKnownScrollPosition = window.scrollY;
 
-        if (headerHeight <= lastKnownScrollPosition){
-            header.classList.add("header--fixed");
-        }
+    if (headerHeight <= lastKnownScrollPosition) {
+      header.classList.add("header--fixed");
+    }
 
-        else{
-            header.classList.remove("header--fixed");
-        }
+    else {
+      header.classList.remove("header--fixed");
+    }
 
-            console.log(lastKnownScrollPosition);
-      });
-
-
+    console.log(lastKnownScrollPosition);
   });
 
 
+});
 
 
-    //   JAVASCRIPT VOOR FOTO SWIPER
-      const swiper = new Swiper('.foto-slider', {
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
 
-      });
 
-      });
-  });
+//   JAVASCRIPT VOOR FOTO SWIPER
+const swiper = new Swiper('.foto-slider', {
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
+
+     
 
