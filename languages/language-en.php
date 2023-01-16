90375cc03ee81b871a2a11fd0d6183d5ba835f29
BenteKavsek <35726@ma-web.nl>
2023-01-16 12:47:09 +0100

    taal opties klaargezet en deels ingevuld.

--- languages/language-en.php
+++ languages/language-en.php
@@ -15,4 +15,49 @@ $translate = [
     "blue_btn_1" => "Buy your tickets here!",
     "blue_btn_2" => "National video game museum",
     "blue_btn_3" => "Zoetermeer, Theaterplein 11",
+
+    "over_title" => "About the National Videogame museum",
+    "over_text" => ".....",
+
+    "geschiedenis_title" => "The museum's history",
+    "geschiedenis_text" => ".....",
+
+    "galerij_title" => ".....",
+    "galerij_text" => ".....",
+
+    "slider_1_text" => ".....",
+    "slider_2_text" => ".....",
+    "slider_3_text" => ".....",
+    "slider_4_text" => ".....",
+    "slider_5_text" => ".....",
+
+    "reviews_title" => "...",
+
+    "review_1_text" => "...",
+    "review_2_text" => "...",
+    "review_3_text" => "...",
+
+    "footer_1_text_1" => "...",
+    "footer_1_btn" => "...",
+    "footer_1_text_2" => "...",
+
+    "footer_2_title" => "...",
+
+    "footer_3_title_1" => "...",
+    "footer_3_title_2" => "...",
+
+    "footer_4_title" => "...",
+    "footer_4_dag_1" => "monday",
+    "footer_4_dag_2" => "tuesday",
+    "footer_4_dag_3" => "wednesday",
+    "footer_4_dag_4" => "thursday",
+    "footer_4_dag_5" => "friday",
+    "footer_4_dag_6" => "zaturday",
+    "footer_4_dag_7" => "sunday",
+    "footer_4_tijdBtn" => "...",
+
+    "footer_copyright" => "Copyright © Nationaal Videogame Museum",
+    "footer_algemeneVoorwaarden" => "Terms and Conditions",
+    "footer_cookieInstellingen" => "Cookie Settings",
+    "footer_privacyVerklaring" => "Privacy Policy",
 ];
--- languages/language-nl-NL.php
+++ languages/language-nl-NL.php
@@ -15,4 +15,49 @@ $translate = [
     "blue_btn_1" => "Koop hier uw tickets!",
     "blue_btn_2" => "Nationaal videogame museum",
     "blue_btn_3" => "Zoetermeer, Theaterplein 11",
+    
+    "over_title" => "Over het Nationaal Videogame museum",
+    "over_text" => ".....",
+
+    "geschiedenis_title" => "De geschiedenis van het museum",
+    "geschiedenis_text" => ".....",
+
+    "galerij_title" => "Wat is er te doen",
+    "galerij_text" => ".....",
+
+    "slider_1_text" => ".....",
+    "slider_2_text" => ".....",
+    "slider_3_text" => ".....",
+    "slider_4_text" => ".....",
+    "slider_5_text" => ".....",
+
+    "reviews_title" => "Reviews over het NVGM",
+
+    "review_1_text" => "...",
+    "review_2_text" => "...",
+    "review_3_text" => "...",
+
+    "footer_1_text_1" => "Schrijf je in voor onze nieuwsbrief en blijf op de hoogte van alles in en rond het mueseum!",
+    "footer_1_btn" => "Schrijf mij in!",
+    "footer_1_text_2" => "Door u aan te melden gaat u akkoord met de algemene voorwaarden van het NVGM.",
+
+    "footer_2_title" => "Navigatie",
+
+    "footer_3_title_1" => "Contact",
+    "footer_3_title_2" => "Volg het NVGM",
+
+    "footer_4_title" => "Openingstijden",
+    "footer_4_dag_1" => "maandag",
+    "footer_4_dag_2" => "dinsdag",
+    "footer_4_dag_3" => "woensdag",
+    "footer_4_dag_4" => "donderdag",
+    "footer_4_dag_5" => "vrijdag",
+    "footer_4_dag_6" => "zaterdag",
+    "footer_4_dag_7" => "zondag",
+    "footer_4_tijdBtn" => "Bekijk Tijdsblokken",
+
+    "footer_copyright" => "Copyright © Nationaal Videogame Museum",
+    "footer_algemeneVoorwaarden" => "Algemene Voorwaarden",
+    "footer_cookieInstellingen" => "Cookie instellingen",
+    "footer_privacyVerklaring" => "Privacy verklaring",
 ];
